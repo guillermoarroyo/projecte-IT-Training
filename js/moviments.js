483bd04c65a4c09922afeae8b87ebb401bb044dd
guillermoarroyo <arroyog477@gmail.com>
2021-02-01 18:10:16 +0100

    funcio passa anterior

--- js/moviments.js
+++ js/moviments.js
@@ -16,3 +16,21 @@ function passaseguent(objecterebut){
     objPregSeg.classList.add("elementVisible");
     objPregSeg.classList.remove("elementOcult");
   }
+  function passaAnterior(objecterebut){
+    let idObjPregAct = objecterebut.parentElement.id;
+    let objPregAct = document.getElementById(idObjPregAct);
+    let colArticles = document.getElementsByTagName("article");
+    let idObjPregAnt;
+
+    for (let i = 0; i < colArticles.length; i++){
+        if (colArticles[i].id == idObjPregAct){
+            idObjPregAnt = colArticles[i-1].id;
+            break;
+        };
+    }
+    let objPregAnt = document.getElementById(idObjPregAnt);
+    objPregAct.classList.remove("elementVisible");
+    objPregAct.classList.add("elementOcult");
+    objPregAnt.classList.add("elementVisible");
+    objPregAnt.classList.remove("elementOcult");
+  }
